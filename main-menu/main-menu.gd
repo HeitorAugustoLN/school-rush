2bc3c411344766f3883cc27ce9b2eb7299988d84
Sávio Cardoso <saviofelipe123456@gmail.com>
2025-09-05 15:07:47 +0000

    feat: add options menu

--- main-menu/main-menu.gd
+++ main-menu/main-menu.gd
@@ -7,3 +7,11 @@ func _on_quit_button_pressed() -> void:
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	$Panel.visible = true
+
+
+func _on_back_pressed() -> void:
+	$Panel.visible = false
