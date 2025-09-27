273e67ec311ddb74cea376b131986cfd74b03478
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-09-26 21:49:54 -0300

    fix: reset score and money when going to main menu

--- game-over/game-over.gd
+++ game-over/game-over.gd
@@ -8,6 +8,8 @@ func _on_yes_button_pressed() -> void:
 
 
 func _on_no_button_pressed() -> void:
+	Score.score = 0
+	Score.money = 0
 	get_tree().change_scene_to_file("res://main-menu/main-menu.tscn")
 
 
